0537cb849cdb6ced538b2656a8d487350bcfd066
esteb37 <esteban37padilla@gmail.com>
2023-05-05 01:24:45 -0600

    First draft

--- train_wm.py
+++ train_wm.py
@@ -1,119 +1,165 @@
 import numpy as np
 from operator import mul
 from T1_set import *
+import matplotlib.pyplot as plt
 
 
 class wang_mendel(object):
-    
-    def __init__(self,time_series, train_data, antecedent_number, past_points):
 
-        self.train_data=train_data
-        self.antecedents=self.generate_antecedents(train_data,antecedent_number)
-        self._p=past_points
-        self.__reduced_rules=self.rule_matrix_generating()
-    
-    def generate_antecedents(self,train_data,antecedent_number):
-            
-            max_number = max(train_data)
-            min_number = min(train_data)
-            
-            antecedents = {}
-            step = ((max_number - min_number) / (antecedent_number - 1)) / 4.0
-                   
-            for i in range(1, antecedent_number + 1):
-                
-                mean = min_number + (i - 1) * step * 4.0
-                if(i == 1):
-                     antecedents[i] = T1_LeftShoulder(mean, step , 500)
-                elif(i == antecedent_number):
-                    antecedents[i] = T1_RightShoulder(mean, step , 500)
-                else:
-                    antecedents[i] = T1_Triangular(mean, step , 500)
-            return (antecedents)
-    
-    
+    def __init__(self, output_type, train_data_matrix, antecedent_number):
+
+        if (output_type not in ['linear', 'angular']):
+            raise Exception("output_type must be 'linear' or 'angular'")
+
+        # Break the input matrix into vectors
+        self.train_distances = train_data_matrix[:, 0]
+        self.train_angles = train_data_matrix[:, 1]
+
+        train_linear_vels = train_data_matrix[:, 2]
+        train_angular_vels = train_data_matrix[:, 3]
+
+        self.train_outputs = train_linear_vels if output_type == 'linear' else train_angular_vels
+
+        self.train_data_matrix = train_data_matrix
+        self.training_size = len(train_data_matrix)
+
+        self.distance_antecedents = self.generate_antecedents(
+            self.train_distances, antecedent_number)
+
+        self.angle_antecedents = self.generate_antecedents(
+            self.train_angles, antecedent_number)
+
+        self.output_antecedents = self.generate_antecedents(
+            self.train_outputs, antecedent_number)
+
+        self.antecedent_number = antecedent_number
+
+        self.__reduced_rules = self.rule_matrix_generating()
+
+    def generate_antecedents(self, training_data, antecedent_number):
+
+        max_value = max(training_data)
+        min_value = min(training_data)
+
+        antecedents = {}
+
+        step = ((max_value - min_value) /
+                (antecedent_number - 1)) / 4.0
+
+        for i in range(1, antecedent_number + 1):
+
+            mean = min_value + (i - 1) * step * 4.0
+            if (i == 1):
+                antecedents[i] = T1_LeftShoulder(
+                    mean, step, 500)
+            elif (i == antecedent_number):
+                antecedents[i] = T1_RightShoulder(
+                    mean, step, 500)
+            else:
+                antecedents[i] = T1_Triangular(
+                    mean, step, 500)
+
+        return (antecedents)
+
     def rule_matrix_generating(self):
-        
-        # For each training record return the x value, membership and number of antecedent with the highest membership
-        x_val_with_mf= self.assign_points()
-        
-        all_rule_matrix = np.zeros([len(self.train_data) - self.p, (self.p + 2)])
-        
-        for i in range(0, len(self.train_data) - self.p):
-            all_rule_matrix[i,0:self.p+1]=np.hstack(x_val_with_mf[i:(i+self.p+1),2:3])
-            
-            #multiplication of degrees
-            #rule_degree = reduce(mul, x_val_with_mf[i:(i+self.p+1),1:2])
-            rule_degree = np.multiply.reduce(x_val_with_mf[i:(i+self.p+1),1:2])
 
-            all_rule_matrix[i,self.p+1:self.p+2]=rule_degree
-            
-        
-        return(self.rule_reduction(all_rule_matrix))
-        #return (all_rule_matrix)
-    
-    def assign_points(self):
-        x_val_with_mf = np.empty([len(self.train_data), (3)])    
-        
-        for index,x in enumerate(self.train_data):
-            x_val_with_mf[index][0]=x
-            x_val_with_mf[index][1:3]=self.get_antIndex_and_maxDegree(x)
-            
-        return(x_val_with_mf) 
-       
-    def get_antIndex_and_maxDegree(self,x):
-        
-        max_degree=0.0
-        
-        for i in self.antecedents:
-            degree=self.antecedents[i].get_degree(x)
-            if(degree>max_degree):
-                max_degree=degree
-                antIndex=i
-        
-    
-        if(max_degree==0.0):
-            raise ValueError( "There is no max degree")
+        # For each training record return the x value, membership and number of antecedent with the highest membership
+        distance_memberships = self.assign_points(
+            self.distance_antecedents, self.train_distances)
+
+        angle_memberships = self.assign_points(
+            self.angle_antecedents, self.train_angles)
+
+        output_memberships = self.assign_points(
+            self.output_antecedents, self.train_outputs)
+
+        # Each rule will have the following shape:
+        # [distance_antecedent, angle_antecedent, output_antecedent, rule_degree]
+        all_rule_matrix = np.zeros(
+            [self.training_size, 4])
+
+        for i in range(self.training_size):
+
+            distance_membership = distance_memberships[i][2]
+            angle_membership = angle_memberships[i][2]
+            output_membership = output_memberships[i][2]
+
+            # The rule degree is obtained by multiplying the membership degrees of the antecedents
+            rule_degree = distance_memberships[i][1] * \
+                angle_memberships[i][1] * \
+                output_memberships[i][1]
+
+            all_rule_matrix[i] = np.array(
+                [distance_membership, angle_membership, output_membership, rule_degree])
+
+        return (self.rule_reduction(all_rule_matrix))
+        # return (all_rule_matrix)
+
+    def assign_points(self, antecedents, training_data):
+        """
+            This function returns an array that contains,
+            for each value "x" in the training data, a touple,
+            with the value itself, the membership degree and the
+            antecedent with the highest membership degree.
+        """
+
+        memberships = np.empty(
+            [len(training_data), 3])
+
+        for index, x in enumerate(training_data):
+            memberships[index][0] = x
+            memberships[index][1:3] = self.get_antIndex_and_maxDegree(
+                antecedents, x)
+
+        return memberships
+
+    def get_antIndex_and_maxDegree(self, antecedents, x):
+
+        max_degree = 0.0
+
+        for i in antecedents:
+            degree = antecedents[i].get_degree(x)
+            if (degree > max_degree):
+                max_degree = degree
+                antIndex = i
+
+        if (max_degree == 0.0):
+            raise ValueError("There is no max degree")
         else:
-            return ((max_degree,antIndex))    
-    
-    
-    def rule_reduction(self,all_rule_matrix):
+            return ((max_degree, antIndex))
+
+    def rule_reduction(self, all_rule_matrix):
 
         for i in range(0, len(all_rule_matrix)):
             temp_rule_1 = all_rule_matrix[i]
-            if not np.isnan(temp_rule_1).any() :
+            if not np.isnan(temp_rule_1).any():
                 for t in range(i + 1, len(all_rule_matrix)):
                     temp_rule_2 = all_rule_matrix[t]
                     # check antecedent equality
                     if np.array_equal(temp_rule_1[0:(self.p)], temp_rule_2[0:(self.p)]):
                         # check degree and keep the greatest
-                        if(temp_rule_2[self.p + 1] > temp_rule_1[self.p + 1]):
+                        if (temp_rule_2[self.p + 1] > temp_rule_1[self.p + 1]):
                             # the rule, with lower degree, is replaced by the one with higher degree
                             all_rule_matrix[i] = all_rule_matrix[t]
                         all_rule_matrix[t] = np.nan
-                       
-        return(all_rule_matrix[~np.isnan(all_rule_matrix).any(axis=1)])
-        
-          
-            
-                       
-    @property
+
+        return (all_rule_matrix[~np.isnan(all_rule_matrix).any(axis=1)])
+
+    @ property
     def mf_interval_matrix(self):
         return self.__mf_interval_matrix
-    
-    @property
+
+    @ property
     def reduced_rules(self):
         return self.__reduced_rules
-    
-    @property
+
+    @ property
     def p(self):
         return self._p
-    
-    @property
+
+    @ property
     def shape(self):
         return self._shape
 
-  
 
-#np.savetxt(" .csv",self.reduced_rules,delimiter=",")
+# np.savetxt(" .csv",self.reduced_rules,delimiter=",")
